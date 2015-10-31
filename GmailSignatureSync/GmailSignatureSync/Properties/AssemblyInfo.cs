3205ba974de32eaf2e55a1c852379461461fed10
MattAlex151 <mattd890@gmail.com>
2015-10-30 20:12:50 -0400

    Scaffolding.

---
+++ GmailSignatureSync/GmailSignatureSync/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GmailSignatureSync")]
+[assembly: AssemblyDescription("Update gmail signatures across your organization.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VuWall Technology, Inc.")]
+[assembly: AssemblyProduct("GmailSignatureSync")]
+[assembly: AssemblyCopyright("VuWall Technology, Inc. Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e59b656a-ab00-46ed-97e9-0ea99dbeeab2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
