1bfdfc0185f163512819056d5a99bf89b39fe0a8
MattAlex151 <mattd890@gmail.com>
2015-10-30 20:25:23 -0400

    Add the necessary NuGet packages to interact with Google APIs

--- GmailSignatureSync/GmailSignatureSync/Program.cs
+++ GmailSignatureSync/GmailSignatureSync/Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.GData.Apps;
+using Google.GData.Apps.GoogleMailSettings;
+using Google.GData.Client;
+using Google.GData.Extensions;
 
 namespace GmailSignatureSync
 {
